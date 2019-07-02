36d46d9267f1b66ad5013e89b96f8e0a942efea7
Tareq Monwer <tareqmonwer137@gmail.com>
2019-07-03 02:16:04 +0600

    jump search algorithm modified

--- searching/JumpSearch/basicJumpSearch.py
+++ searching/JumpSearch/basicJumpSearch.py
@@ -1,22 +1,24 @@
-arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
-val = 55
+import math
 
-block = 4
+arr = [12, 133, 244, 246, 455, 567, 677, 788]
+val = 455
+
 start = 0
 pos = 0
-last = len(arr) - 1
+last = len(arr)
+block = math.sqrt(last)
 
-while val != arr[pos]:
-    if val > arr[pos]:
+while val != arr[int(pos)]:
+    if val > arr[int(pos)]:
         pos -= block
-        while val != arr[pos]:
+        while val != arr[int(pos)]:
             pos += 1
 
         
-    elif val < arr[pos]:
+    elif val < arr[int(pos)]:
         pos += block
 
-if val == arr[pos]:
-    print('%d found in index: %d' %(arr[pos], pos))
+if val == arr[int(pos)]:
+    print('%d found in index: %d' %(arr[int(pos)], pos))
 else:
     print(-1)
